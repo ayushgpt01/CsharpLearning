6535b72d0833c11c56179069bc1082b4a7cc156c
ayushgpt01 <ayushgpt043@gmail.com>
2022-03-04 23:49:16 +0530

    loops added

---
+++ Basics/Loops.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            // while loop
+            Console.WriteLine("While Loop :- ");
+            while(i < 10)
+            {
+                Console.WriteLine("Index : " + ++i);
+            }
+            i = 0;
+            // do-while
+            Console.WriteLine("Do - While Loop :- ");
+
+            do
+            {
+                Console.WriteLine("Index : "+ ++i);
+            } while(i < 10);
+
+            //for loop
+            Console.WriteLine("For Loop :- ");
+
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Index : "+ i);
+            }
+        }
+    }
+}
