c8219d27268f1b4fb5335a9366bcfb284227bddb
ayushgpt01 <ayushgpt043@gmail.com>
2022-03-04 12:42:03 +0530

    Adding new filenames

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number : ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Addition : " + (num1 + num2));
+            Console.WriteLine("Subtraction : " + (num1 - num2));
+            Console.WriteLine("Multiplication : " + (num1 * num2));
+            Console.WriteLine("Division : " + (num1 / num2));
+            Console.WriteLine("     Remainder left : " + (num1 % num2));
+        }
+    }
+}
