4ccc5cb7751b5660fe3e7f1482160bb0b9d2e4c4
ayushgpt01 <ayushgpt043@gmail.com>
2022-03-04 20:48:42 +0530

    Calc v2

---
+++ Calculator/CalculatorV2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number : ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the operator (+,-,*,/,%): ");
+            string oper = Console.ReadLine();
+
+            if(oper == "+")
+            {
+                Console.WriteLine("Addition : " + (num1 + num2));
+            } else if(oper == "-")
+            {
+                Console.WriteLine("Subtraction : " + (num1 - num2));
+            } else if(oper == "*")
+            {
+                Console.WriteLine("Multiplication : " + (num1 * num2));
+            }
+            else if (oper == "/")
+            {
+                Console.WriteLine("Division : " + (num1 / num2));
+            }
+            else if (oper == "%")
+            {
+                Console.WriteLine("     Remainder left : " + (num1 % num2));
+            } else
+            {
+                Console.WriteLine("Incorrect input - Invalid Operator");
+            }
+        }
+    }
+}
