e4707d666a49aa192360e6f0643e98618a65ef9e
ayushgpt01 <ayushgpt043@gmail.com>
2022-03-05 14:09:35 +0530

    Exception Handling

---
+++ Basics/ExceptionHandling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter a number : ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number : ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 / num2);
+            } catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            } catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            } finally
+            {
+                Console.WriteLine("This Block always runs but is optional to define");
+            }
+            
+        }
+    }
+}
